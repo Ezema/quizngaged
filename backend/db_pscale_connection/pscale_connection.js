b6bcc53f34b21bc6ceb24dfba069b9e6a4573cf4
ezema <ezequielmartintorres@hotmail.com>
2022-01-28 21:06:55 +0100

    Tsk 006 test pscale access

---
+++ backend/db_pscale_connection/pscale_connection.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+connection.connect()
+
+app.get('/', (req, res) => {
+  connection.query('SHOW TABLES', function (err, rows, fields) {
+    if (err) throw err
+
+    res.send(rows)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
