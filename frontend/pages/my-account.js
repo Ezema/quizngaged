74e29ded3143e4f0bfbfe6adab3217391271517c
Ezekiel <ezemartint@gmail.com>
2022-02-01 15:36:22 +0100

    '/my-account' page now loads the data from the server

--- frontend/pages/my-account.js
+++ frontend/pages/my-account.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 
 import Skeleton from '@mui/material/Skeleton'
-import { Container } from '@mui/material';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 import CustomTopNavBar from '../customComponents/customTopNavBar'
 
@@ -9,29 +10,77 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import firebaseClientConfig from '../customGlobalVariables/firebaseClientConfig';
 
+import backendQueryGetUserJSON from '../customFunctions/backendQueries/backendQueryGetUserJSON';
+
+import Typography from '@mui/material/Typography';
+
 export default function MyClassrooms(props) {
 
   const [statefulUserObject, setStatefulUserObject] = React.useState({});
   const [topBarTitle,setTopBarTitle] = React.useState("My Account")
 
-  firebase.initializeApp(firebaseClientConfig);    
-  firebase.app()
-  firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-      setStatefulUserObject(user)
+  const [statefulQuizngagedUserData, setStatefulQuizngagedUserData] = React.useState({});
+
+  React.useEffect(()=>{
+    if(window.location.pathname.localeCompare("/my-account")!=0){
+      window.location.pathname = "/my-account"
     }
-  })
+    if((localStorage.federatedAuthUserData)==null || localStorage.federatedAuthUserData==undefined){
+      router.push('/')
+    }else if(localStorage.quizngagedUserData==null || localStorage.quizngagedUserData==undefined){
+      router.push('/')
+    }else{
+      backendQueryGetUserJSON({callback:setStatefulQuizngagedUserData})
+    }
+    
+  },[])
 
   return (
     <div>
-    <CustomTopNavBar statefulUserObject={statefulUserObject} setStatefulUserObject={setStatefulUserObject} topBarTitle={topBarTitle} setTopBarTitle={setTopBarTitle}></CustomTopNavBar>
-    <Container>
-      <Skeleton marginTop='2em' animation="wave" variant='h1'/>
-      <Skeleton marginTop='2em' animation="wave" variant='h1'/>
-      <Skeleton marginTop='2em' animation="wave" variant='h1'/>
-      <Skeleton marginTop='2em' animation="wave" variant='h1'/>
-      <Skeleton marginTop='2em' animation="wave" variant='h1'/>
-    </Container>
+      <Grid Container display={'grid'}>
+        <Grid item>
+          <CustomTopNavBar statefulUserObject={statefulUserObject} setStatefulUserObject={setStatefulUserObject} topBarTitle={topBarTitle} setTopBarTitle={setTopBarTitle}></CustomTopNavBar>          
+        </Grid>
+        <Grid item marginTop={'2em'}>
+          <Grid Container display={'grid'} justifyContent={'center'} textAlign={'center'}>            
+            <Grid item justifyContent={'center'}>
+              {(statefulQuizngagedUserData.uid==undefined)?
+              (
+                <Skeleton marginTop='2em' animation="wave" variant='h1'/>
+              )
+              :
+              (
+                <Typography>{statefulQuizngagedUserData.uid}</Typography>
+              )}
+            </Grid>
+            <Grid item paddingTop={'1em'} justifyContent={'center'} textAlign={'center'}>
+              {(statefulQuizngagedUserData.name==undefined)?
+              (
+                <Skeleton marginTop='2em' animation="wave" variant='h1'/>
+              )
+              :
+              (
+                <Typography>{statefulQuizngagedUserData.name}</Typography>
+              )}
+            </Grid>
+            <Grid item paddingTop={'1em'} justifyContent={'center'} textAlign={'center'}>
+              {(statefulQuizngagedUserData.email==undefined)?
+              (
+                <Skeleton marginTop='2em' animation="wave" variant='h1'/>
+              )
+              :
+              (
+                <Typography>{statefulQuizngagedUserData.email}</Typography>
+              )}
+            </Grid>
+          </Grid>
+        </Grid>
+        
+          
+
+      </Grid>
+
+    
     </div>
   );
 }
