2677e29bf4912b98b0046ae9bd6a78280add34d9
Ezekiel <ezemartint@gmail.com>
2022-02-03 17:45:24 +0100

    teacher can now see a list of ongoing quizzes

--- frontend/customComponents/viewClassroomOngoingQuizzes.js
+++ frontend/customComponents/viewClassroomOngoingQuizzes.js
@@ -1,9 +1,55 @@
 import * as React from 'react';
 
-export default function ViewClassroomOngoingQuizzes(){
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
+import CustomPaperReactComponent from './customPaperReactComponent.js';
+
+export default function ViewClassroomOngoingQuizzes(props){
+
+    const handleViewLiveOngoingQuiz = ()=>{}
     return(
         <div>
-            hey
+            <Container>
+                {(JSON.parse(localStorage.quizngagedUserData).classrooms==undefined)?
+                (
+                    <div></div>
+                )
+                :
+                (JSON.parse(localStorage.quizngagedUserData).classrooms[props.viewClassroomUID].ongoingLiveQuizzes.length==0)?
+                (
+                    <div>empty</div>
+                )
+                :
+                (
+                    <Box paddingTop="1em" paddingBottom="100px">
+                    <Grid container spacing={2}>                        
+                    {
+                        JSON.parse(localStorage.quizngagedUserData).classrooms[props.viewClassroomUID].ongoingLiveQuizzes.map((ongoingLiveQuiz)=>
+                        <Grid item xs={12} md={6} lg={4} key={JSON.parse(localStorage.quizngagedUserData).classrooms[props.viewClassroomUID].ongoingLiveQuizzes.indexOf(ongoingLiveQuiz)}>
+                            <CustomPaperReactComponent elevation={3}>                            
+                            <Typography variant='h5' inline>
+                                #{ongoingLiveQuiz.id+" "+"Event description: "+ongoingLiveQuiz.eventDescription}
+                            </Typography>                      
+                            <Typography variant='subtitle1'>
+                                {"Quiz ID in use: "+ongoingLiveQuiz.quizSelected}
+                            </Typography>                            
+                            {/* <Typography variant='subtitle1'>
+                                {"Quiz Title in use: "+ongoingLiveQuiz.quizTitle}
+                            </Typography>*/}                            
+                            <Button size="small" onClick={(event) => handleViewLiveOngoingQuiz(event, ongoingLiveQuiz.id)}>VIEW</Button>
+                            {/* <Button size="small" onClick={(event) => handleEditClassroom(event, classroom.id)}>EDIT</Button> */}
+                            </CustomPaperReactComponent>
+                        </Grid>      
+                        )
+                    }
+                    </Grid>              
+                    </Box>
+                )}
+            </Container>
         </div>
     )
 }
