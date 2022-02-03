52fa54592c7056453d0144af6ef2a80051663cf4
Ezekiel <ezemartint@gmail.com>
2022-02-03 15:05:08 +0100

    second 'view classroom' draft

--- frontend/customComponents/viewClassroom.js
+++ frontend/customComponents/viewClassroom.js
@@ -4,8 +4,11 @@ import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+
+
 import BoltIcon from '@mui/icons-material/Bolt';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import BarChartIcon from '@mui/icons-material/BarChart';
 
 export default function ViewClassroom(props){
     return(
@@ -14,26 +17,37 @@ export default function ViewClassroom(props){
             <Grid Container display={'grid'}>
                 <Grid item marginBottom={'1em'}>
                     <Paper elevation={3}>
-                        <Grid Container display={'flex'} justifyContent={'center'} alignContent={'center'} alignItems={'center'}>
-                            <Grid item margin={'0.1em'} textAlign={'center'}>
-                                <BoltIcon color="primary"/>
+                        <Grid Container display={'flex'} /* justifyContent={'center'}  */alignContent={'center'} alignItems={'center'}>
+                            <Grid item textAlign={'center'} sx={{background:'#1976d2',padding:'1em'}}>
+                                <BoltIcon sx={{ fontSize: 100, color:'white'}}/>
                             </Grid>
-                            <Grid item margin={'0.1em'} textAlign={'center'}>
+                            <Grid item right={'0.1em'}>
                                 <Button>
                                     Launch quiz
                                 </Button>
+                            </Grid>                            
+                        </Grid>
+                    </Paper>
+                </Grid>
+                <Grid item marginBottom={'1em'} /* minHeight={'30vh'} */>
+                    <Paper elevation={3}>
+                        <Grid Container display={'flex'} alignContent={'center'} alignItems={'center'} /* minHeight={'30vh'} */>
+                            <Grid item textAlign={'center'} left={'0.1em'} sx={{background:'#1976d2',padding:'1em'}}>
+                                <VisibilityIcon sx={{fontSize:100, color:'white'}}/>
                             </Grid>
-                            <Grid item margin={'0.1em'} textAlign={'center'}>
-                                <BoltIcon color="primary"/>
+                            <Grid item margin={'0.1em'} textAlign={'center'} >
+                                   <Button>
+                                    View results
+                                </Button>
                             </Grid>
                         </Grid>
                     </Paper>
                 </Grid>
-                <Grid item marginBottom={'1em'}>
+                <Grid item>
                     <Paper elevation={3}>
-                        <Grid Container display={'flex'} justifyContent={'center'} alignContent={'center'} alignItems={'center'}>
-                            <Grid item margin={'0.1em'} textAlign={'center'}>
-                                <VisibilityIcon color="secondary"/>
+                        <Grid Container display={'flex'} alignContent={'center'} alignItems={'center'}>
+                            <Grid item textAlign={'center'} left={'0.1em'} sx={{background:'#1976d2',padding:'1em'}}>
+                                <BarChartIcon sx={{fontSize:100, color:'white'}}/>
                             </Grid>
                             <Grid item margin={'0.1em'} textAlign={'center'} >
                                    <Button>
