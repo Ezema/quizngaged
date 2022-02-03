165c92e7f997cb7388f25b70042de4a8a7d716ef
ezema <ezequielmartintorres@hotmail.com>
2022-02-03 19:13:56 +0100

    user can't access unauthorized pages '/my-questions' and '/my-quizzes'

---
+++ frontend/userAccess/userAccess.js
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router'
+
+import LoadingScreen from '../customComponents/loadingScreen'
+
+export const UserAccessCheck = (props) => {
+  
+   
+  return props.children
+}
