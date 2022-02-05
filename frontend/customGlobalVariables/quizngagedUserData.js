99c1752e7cac279fa58161bab861e9d2309a970f
ezema <ezequielmartintorres@hotmail.com>
2022-02-05 11:46:44 +0100

    teacher new account starts with zero classrooms

--- frontend/customGlobalVariables/quizngagedUserData.js
+++ frontend/customGlobalVariables/quizngagedUserData.js
@@ -7,29 +7,7 @@ export default {
         "photoURL":null,
         "name":null,
         "phone":null,
-        "classrooms":[
-            {
-                id:0,
-                name:'My first classroom',
-                isDeleted:false,
-                pastQuizzes:[                
-                ],
-                ongoingLiveQuizzes:[                
-                ],
-                classroomStatistics:{},
-                classroomSettings:{}
-            },
-            {
-                id:1,
-                isDeleted:false,
-                name:'My Second classroom',
-                pastQuizzes:[                    
-                ],
-                ongoingLiveQuizzes:[                
-                ],
-                classroomStatistics:{},
-                classroomSettings:{}
-            }
+        "classrooms":[            
         ],
         "quizzes":
         [
