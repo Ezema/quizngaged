78d6dca544fae68878e3c7c0c3acedef61829713
Tomas Stubbs <tomas.stubbs@gmail.com>
2022-02-12 12:56:53 -0800

    TSK-036: amend add/editQuestion to use form validator

--- frontend/customComponents/addQuestion.js
+++ frontend/customComponents/addQuestion.js
@@ -51,6 +51,7 @@ import QuestionAnswers from './questionAnswer.js';
 import Autocomplete from '@mui/material/Autocomplete';
 
 import backendQuerySaveUserJSON from '../customFunctions/backendQueries/backendQuerySaveUserJSON.js';
+import * as formValidator from '../customFunctions/formValidation.js';
 
 import { width } from '@mui/system';
 
@@ -66,8 +67,13 @@ export default function AddQuestion(props){
     const setListOfQuestions = props.setListOfQuestions;
     const newQuestionUID = props.newQuestionUID;
 
+    const questionTypes = ['Multiple Choice', 'Text Response', 'Binary Question'];
+    const multipleChoiceQuestionIdx = 0;
+    const textQuestionIdx = 1;
+    const binaryQuestionIdx = 2;
+
     const newQuestion = {id:newQuestionUID,
-        questionType:'Multiple Choice',
+        questionType: questionTypes[multipleChoiceQuestionIdx],
         questionBaselineBody:"",
         questionBaselineAnswers:[{id:1,parentQuestionId:newQuestionUID,body:'', isCorrect:false}],
         questionEasierBody:"",
@@ -96,29 +102,27 @@ export default function AddQuestion(props){
     const [userEntryHarderQuestionBody,setUserEntryHarderQuestionBody] = React.useState(statefulNewQuestion.questionHarderBody)
 
     const handleQuestionBodyChange = (event, questionDifficulty)=>{
-        let regExp = /^\s*$/;      
-        let changedBody = event.target.value;
-        let isEmpty = regExp.test(changedBody);
-        if(questionDifficulty==0){  
-            setUserEntryBaselineQuestionBody(changedBody)
-            setEntriesAreValid(changedBody.length != 0 && !isEmpty);
-        }else if(questionDifficulty==1){
-            setUserEntryEasierQuestionBody(changedBody)
-            if(statefulArrayOfQuestionAnswers.length == 0){
-                setEntriesAreValid(true);
-            }else{
-                setEntriesAreValid(changedBody.length != 0 && !isEmpty);
-            }
-        }else if(questionDifficulty==2){
-            setUserEntryHarderQuestionBody(changedBody)
-            if(statefulArrayOfQuestionAnswers.length == 0){
-                setEntriesAreValid(true);
-            }else{
-                setEntriesAreValid(changedBody.length != 0 && !isEmpty);
-            }
-        }
-    }
-
+      let changedBody = event.target.value;
+      let isValid = formValidator.isValidMandatoryText(changedBody);
+      if(questionDifficulty==0){  
+          setUserEntryBaselineQuestionBody(changedBody)
+          setEntriesAreValid(isValid);
+      }else if(questionDifficulty==1){
+          setUserEntryEasierQuestionBody(changedBody)
+          if(statefulArrayOfQuestionAnswers.length == 0){
+              setEntriesAreValid(true);
+          }else{
+              setEntriesAreValid(isValid);
+          }
+      }else if(questionDifficulty==2){
+          setUserEntryHarderQuestionBody(changedBody)
+          if(statefulArrayOfQuestionAnswers.length == 0){
+              setEntriesAreValid(true);
+          }else{
+              setEntriesAreValid(isValid);
+          }
+      }
+  }
 
     const calculateLastQuestionAnswerUID = (array)=>{        
         if(array.length>0){
@@ -199,31 +203,31 @@ export default function AddQuestion(props){
 
         let answers = statefulArrayOfQuestionAnswers
 
-        if(userEntryBaselineQuestionBody.length == 0){
+        if(!formValidator.isValidMandatoryText(userEntryBaselineQuestionBody)){
             return false;
         }
 
-        if(userEntryQuestionType == '' || (userEntryQuestionType !== 'Text Response' && userEntryQuestionType !== 'Multiple Choice' && userEntryQuestionType !== 'Binary Question')){
-            setSnackBar({isOpen:true, message:"Question type is wrong", severity:"error"}) 
+        if(!formValidator.isValidEntryChoice(userEntryQuestionType, questionTypes)) {
+            setSnackBar({isOpen:true, message:"Question type is invalid", severity:"error"}) 
             setQuestionTypeEntriesAreValid(false);
             return false;
         }
 
-        if(userEntryQuestionType == 'Text Response'){
-           // no need for option checking 
-           return true;
+        if(userEntryQuestionType == questionTypes[textQuestionIdx]){
+            // no need for option checking 
+            return true;
         }
 
-        if(userEntryQuestionType == 'Multiple Choice' ){
+        if(userEntryQuestionType == questionTypes[multipleChoiceQuestionIdx]){
           if(answers.length<2){
-             setSnackBar({isOpen:true, message:"Multichoice questions should have at least 2 answer variants", severity:"error"}) 
+             setSnackBar({isOpen:true, message:"Multichoice questions should have at least 2 possible answers", severity:"error"}) 
              return false;
         }
           }
    
-        if  (userEntryQuestionType == 'Binary Question'){
+        if (userEntryQuestionType == questionTypes[binaryQuestionIdx]){
            if(answers.length!=2){
-             setSnackBar({isOpen:true, message:"Binary questions should have exactly 2 answer variants", severity:"error"}) 
+              setSnackBar({isOpen:true, message:"Binary questions should have exactly 2 possible answers", severity:"error"}) 
               return false;
           } 
         }
@@ -240,7 +244,7 @@ export default function AddQuestion(props){
           }
         }
         if(!correctAnswerIsSet){
-              setSnackBar({isOpen:true, message:"Please pick up correct answer", severity:"error"})
+              setSnackBar({isOpen:true, message:"Please select the correct answer", severity:"error"})
               return false
         }
         
@@ -254,9 +258,9 @@ export default function AddQuestion(props){
 
     const handleNextStep = ()=>{
         /* if(step<2 && entriesAreValid){ */
-            let quizBodyNotEntered = (userEntryBaselineQuestionBody == undefined || userEntryBaselineQuestionBody.length == 0);
-            let easierQuizBodyNotEntered = (userEntryEasierQuestionBody == undefined || userEntryEasierQuestionBody.length == 0);
-            let harderQuizBodyNotEntered = (userEntryHarderQuestionBody == undefined || userEntryHarderQuestionBody.length == 0);
+            let quizBodyNotEntered = !formValidator.isValidMandatoryText(userEntryBaselineQuestionBody);
+            let easierQuizBodyNotEntered = !formValidator.isValidMandatoryText(userEntryEasierQuestionBody);
+            let harderQuizBodyNotEntered = !formValidator.isValidMandatoryText(userEntryHarderQuestionBody);
             if(step==0){
                 if(!isQuestionValid()){
                     if(quizBodyNotEntered){
@@ -363,8 +367,6 @@ export default function AddQuestion(props){
                 // call the backend to sync the local changes
                 backendQuerySaveUserJSON(()=>{})
 
-
-
                 setAddQuestionState(false)
                 setStep(0)
             }           
@@ -374,8 +376,6 @@ export default function AddQuestion(props){
         
     }
 
-    const questionType = ['Multiple Choice','Text Response', 'Binary Question']    
-    
     return(
         <div>   
             <Stepper activeStep={step}>
@@ -441,23 +441,23 @@ export default function AddQuestion(props){
                                 setUserEntryQuestionType(newInputValue)
                                 }}
                                 disablePortal
-                                options={questionType}
+                                options={questionTypes}
                                 fullWidth
                                 renderInput={(questionOption) => <TextField {...questionOption} 
                                 label="Question Type" 
                                 required
                                 error={!questionTypeEntriesAreValid}
-                                helperText={questionTypeEntriesAreValid?'':"Question type is wrong"}
+                                helperText={questionTypeEntriesAreValid?'':"Question type is invalid"}
                             />}
                             />
                         </Box>
-                         { (userEntryQuestionType == 'Multiple Choice' || userEntryQuestionType =='Binary Question') && 
+                        { (userEntryQuestionType == questionTypes[multipleChoiceQuestionIdx] || userEntryQuestionType == questionTypes[binaryQuestionIdx]) && 
                         <div>
                             <Box marginBottom="0.1em">
                             <QuestionAnswers  statefulArrayOfQuestionAnswers={statefulArrayOfQuestionAnswers} setStatefulArrayOfQuestionAnswers={setStatefulArrayOfQuestionAnswers}>
                             </QuestionAnswers>    
-                            </Box>                  
-                            { ( (userEntryQuestionType =='Binary Question' && statefulArrayOfQuestionAnswers.length < 2) || userEntryQuestionType =='Multiple Choice'  ) &&
+                            </Box>
+                            { (statefulArrayOfQuestionAnswers.length < 2 || userEntryQuestionType == questionTypes[multipleChoiceQuestionIdx]) &&
                              <Box marginBottom="2em" textAlign={'center'}>
                                 <Button size='medium' variant='contained' color='secondary' startIcon={<AddIcon/>} onClick={handleNewQuestionAnswer}>
                                     Add answer
