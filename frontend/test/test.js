bfbf8ad455e8c566a739be0265c335088ade7a6c
Tomas Stubbs <tomas.stubbs@gmail.com>
2022-02-12 12:28:07 -0800

    TSK-036: extend form validation and tests to include choosing an element from an array

--- frontend/test/test.js
+++ frontend/test/test.js
@@ -63,4 +63,67 @@ describe("Set of tests", () => {
     });
   });
 
+  describe("Check option choice with no arguments fails", () => {
+    let isValid = validator.isValidEntryChoice();
+    it("should fail with no arguments", () => {
+      assert.equal(isValid, false);
+    });
+  });
+
+  describe("Check array for option choice is not null", () => {
+    let isValid = validator.isValidEntryChoice('x', null);
+    it("should fail with null array", () => {
+      assert.equal(isValid, false);
+    });
+  });
+
+  describe("Check option choice from array is not null", () => {
+    let isValid = validator.isValidEntryChoice(null, [1,2,3]);
+    it("should fail with just one parameter", () => {
+      assert.equal(isValid, false);
+    });
+  });
+
+  describe("Check array for option choice is not empty", () => {
+    let isValid = validator.isValidEntryChoice('x', []);
+    it("should fail with zero length array", () => {
+      assert.equal(isValid, false);
+    });
+  });
+
+  describe("Check option choice is not in the array", () => {
+    let isValid = validator.isValidEntryChoice('x', ['y']);
+    it("should fail with invalid choice", () => {
+      assert.equal(isValid, false);
+    });
+  });
+
+  describe("Check option choice is only element in the array", () => {
+    let isValid = validator.isValidEntryChoice('x', ['x']);
+    it("should pass with valid choice", () => {
+      assert.equal(isValid, true);
+    });
+  });
+
+  describe("Check option choice is not first element in the array", () => {
+    let isValid = validator.isValidEntryChoice('x', ['y', 'x']);
+    it("should pass with valid choice", () => {
+      assert.equal(isValid, true);
+    });
+  });
+
+  describe("Check option choice is not first or last element in the array", () => {
+    let isValid = validator.isValidEntryChoice('x', ['y', 'x', 'z']);
+    it("should pass with valid choice", () => {
+      assert.equal(isValid, true);
+    });
+  });
+
+  describe("Check option choice is not in the array with many elements", () => {
+    let isValid = validator.isValidEntryChoice('x', ['y', 'a', 'z', 'b']);
+    it("should fail with invalid choice", () => {
+      assert.equal(isValid, false);
+    });
+  });
+
 });
