dca309a8d33e9b23c38708e49cfc1e16708f444d
Xu Peng Yan <xupeng.yan@gmail.com>
2022-02-12 20:44:44 -0800

    installed cypress, wrote one test to login a registered user

--- frontend/cypress/integration/test-one.js
+++ frontend/cypress/integration/test-one.js
@@ -1,6 +1,20 @@
 describe('homepage loads', () =>{
-    it('renders correctly', ()=>{
+    it('allows registration as a teacher', ()=>{
         cy.visit('/')
+        cy.findByRole('button', {  name: /sign in with email/i}).click()
+        cy.get('#firebaseui_container > div > form > div:nth-child(2) > div > div:nth-child(1)').type('hello@Cypress.io')
+        cy.findByRole('button', {name: /next/i}).click()
+        cy.findByLabelText(/password/i).type('testpassword1234')
+        cy.findByRole('button', {name: /sign in/i}).click()
+        cy.findByRole('button', {  name: /create teacher acccount/i}).click()
+        cy.findByRole('button', {  name: /next/i}).click()
+        cy.findByRole('button', { name: /confirm/i}).click()
+        
+        // cy.findByRole('textbox', {name: /name/i}).type('firstname lastname') // fails at being able to edit name
+        // cy.findByRole('textbox', {  name: /phone/i}).type(123-456-7890) // fails at being able to edit phone number
+        // cy.findByRole('button', {  name: /next/i}).click()
+        // cy.findByRole('button', {  name: /confirm/i}).click()
+        // cy.get('#__next > div > div > div') 
     })
     
 })
