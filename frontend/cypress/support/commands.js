9572f84504f6661113f91ba60b0c9f41143ccd18
Xu Peng Yan <xupeng.yan@gmail.com>
2022-02-13 17:43:49 -0800

    TSK-038a cypress now has some add on functions for use during testing: clears all table entries, clears indexedDB

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -25,3 +25,24 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import '@testing-library/cypress/add-commands';
 
+Cypress.Commands.add("clear_mysql_and_indexedDB", () => {
+    cy.task('queryDb', 'DELETE FROM users;').then(result => {
+        console.log('dropped all users entries drom sql database')
+    })
+    cy.task('queryDb', 'DELETE FROM classrooms;').then(result => {
+        console.log('dropped all classrooms entries in sql database')
+    })
+    var dbDeleteRequest = window.indexedDB.deleteDatabase("firebaseLocalStorageDb");
+    dbDeleteRequest.onerror= function(event) {
+        console.log("error deleting indexedDB.")
+    }
+    dbDeleteRequest.onsuccess = function(event) {
+        console.log("indexedDB deleted successfully")
+    }
+    const dbs = window.indexedDB.databases()
+    dbs.then(databases => {
+        if (databases.length == 0) {
+            console.log("the indexedDB firebaseLocalStorageDb is empty")
+        } //should log an empty array
+    }) 
+})
