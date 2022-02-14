b224c07155ad681b3e534e0f886e9e9e41470b1d
Xu Peng Yan <xupeng.yan@gmail.com>
2022-02-13 17:43:49 -0800

    TSK-038a cypress now has some add on functions for use during testing: clears all table entries, clears indexedDB

--- frontend/cypress/integration/teacher_can_login.js
+++ frontend/cypress/integration/teacher_can_login.js
@@ -1,25 +1,5 @@
 describe('homepage loads', () =>{
     beforeEach(()=>{
-        // cy.task('queryDb', 'DELETE FROM users;').then(result => {
-        //     console.log('dropped all users entries drom sql database')
-        // })
-        // cy.task('queryDb', 'DELETE FROM classrooms;').then(result => {
-        //     console.log('dropped all classrooms entries in sql database')
-        // })
-        // var dbDeleteRequest = window.indexedDB.deleteDatabase("firebaseLocalStorageDb");
-        // dbDeleteRequest.onerror= function(event) {
-        //     console.log("error deleting indexedDB.")
-        // }
-        // dbDeleteRequest.onsuccess = function(event) {
-        //     console.log("indexedDB deleted successfully")
-        // }
-        // const dbs = window.indexedDB.databases()
-        // dbs.then(databases => {
-        //     if (databases.length == 0) {
-        //         console.log("the indexedDB firebaseLocalStorageDb is empty")
-        //     } //should log an empty array
-        // }) 
-        // a more neatly packaged function than the commands above, described in ../support/commands.js : 
         cy.clear_mysql_and_indexedDB();
     })
     it('allows registration as a teacher', ()=>{
