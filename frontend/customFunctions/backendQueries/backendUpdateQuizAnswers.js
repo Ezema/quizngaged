b26f9c665468613a1999b0c442d43d553c459cd0
Tomas Stubbs <tomas.stubbs@gmail.com>
2022-02-15 12:58:27 -0800

    TSK-041: add saving student answers to backend ;

--- frontend/customFunctions/backendQueries/backendUpdateQuizAnswers.js
+++ frontend/customFunctions/backendQueries/backendUpdateQuizAnswers.js
@@ -1,25 +1,24 @@
 const axios = require('axios');
 
-export default function backendQueryGetQuiz(quizId,props){
-    console.log("inside backendQueryGetQuiz quizId="+quizId);
+export default function backendUpdateQuizAnswers(userquizzid, answersjson){
+    console.log("inside backendUpdateQuizAnswers userquizzid="+userquizzid);
 
   if(JSON.parse(localStorage.federatedAuthUserData)!=null && JSON.parse(localStorage.federatedAuthDecodedToken)!=null){        
     console.log("sending update")
     axios({
       method: "POST",        
-      url: 'http://localhost:9090/API/studentjoinquiz', 
+      url: 'http://localhost:9090/API/updatestudentquiz',
       data: {            
           //the idToken is only for Firebase, it is used to check that the user is authentic and not a bot.
           federatedAuthDecodedToken:JSON.parse(localStorage.federatedAuthDecodedToken),
           //the id or uid is our internal id for each user in the database of quizngaged app.
           uid:JSON.parse(localStorage.federatedAuthUserData).uid,  
-          quizId:quizId
+          userquizzid:userquizzid, 
+          answersjson:answersjson
       },
       timeout:10000      
     }).then(async (response) => {
-
-      props.callback(response.data.joinedQuiz, response.data.studentAnswers);
-        
+      return;
     }).catch(e => {          
         console.log(e);                                
     })
