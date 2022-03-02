fb30592c85d063bf6eb6b3322c4027e9f15bef30
intr3pids <ezemartint@gmail.com>
2022-03-02 17:51:42 +0100

    trying new static server

--- frontend/customFunctions/backendQueries/backendQueryGetUserJSON.js
+++ frontend/customFunctions/backendQueries/backendQueryGetUserJSON.js
@@ -13,7 +13,7 @@ export default function backendQueryGetUserJSON(props){
 
         axios({
             method: "POST",            
-            url: `${process.env.NEXT_PUBLIC_BACKEND_HOST}:9090/API/getuserJSON`,
+            url: `${process.env.NEXT_PUBLIC_BACKEND_HOST}/API/getuserJSON`,
             data: {            
                 //the idToken is only for Firebase, it is used to check that the user is authentic and not a bot.
                 federatedAuthDecodedToken:JSON.parse(localStorage.federatedAuthDecodedToken),
