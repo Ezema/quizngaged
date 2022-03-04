71100f54d1fba1e4a8780c0cd1bfcbb4a810cc60
ezema <ezequielmartintorres@hotmail.com>
2022-03-04 11:02:46 +0100

    HTTPS redirects to HTTP

--- backend/static-frontend-server.js
+++ backend/static-frontend-server.js
@@ -1,9 +1,6 @@
 const httpProxy = require('http-proxy');
 var https = require('https');
-var http = require('http');
 var fs = require('fs');
-var express = require('express')
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 httpProxy.createServer({
   target: {
@@ -12,41 +9,13 @@ httpProxy.createServer({
   }
 }).listen(80);
 
-/*
-httpProxy.createServer({
-  target: {
-    host: 'localhost',
-    port: 3000
-  },
-  ssl: {
-    key: fs.readFileSync('/etc/letsencrypt/live/www.quizngaged.ml/privkey.pem', 'utf8'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/www.quizngaged.ml/fullchain.pem', 'utf8'),    
-  }
-}).listen(443);
-
 const credentials = {
   key: fs.readFileSync('/etc/letsencrypt/live/www.quizngaged.ml/privkey.pem', 'utf8'),
   cert: fs.readFileSync('/etc/letsencrypt/live/www.quizngaged.ml/fullchain.pem', 'utf8'),    
 };
 
-const app = express()
-
-app.use('/', createProxyMiddleware({ target: {host:'www.quizngaged.ml',port:3000}, changeOrigin: true, headers: { "Connection": "keep-alive"} }));
-//app.use('/API', createProxyMiddleware({ target: 'localhost:9000', changeOrigin: true }));
-
-// Starting both http & https servers
-const httpServer = http.createServer(function (req, res) {
-  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+//redirect https to http
+const httpsServer = https.createServer(credentials,function (req, res) {
+  res.writeHead(301, { "Location": "http://" + req.headers['host'] + req.url });
   res.end();
-});
-
-const httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(80, () => {
-console.log('HTTP Server running on port 80');
-});
-
-httpsServer.listen(443, () => {
-console.log('HTTPS Server running on port 443');
-});
-*/
+}).listen(443);
